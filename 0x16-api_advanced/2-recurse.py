b4007628e3c7e1aeab32b630e2162c5f39ad5d19
bryanbuiles <1482@holbertonschool.com>
2020-09-17 16:10:29 -0500

    all tasks

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,15 +10,18 @@ def recurse(subreddit, hot_list=[], aftervalue="aftervalue"):
         url = "https://www.reddit.com/r/" + subreddit + "/hot/.json?after=" \
             + aftervalue
     html = requests.get(url, headers=header, allow_redirects=False)
-    if html.status_code == 200:
-        after = html.json().get("data").get("after")
-        for i in html.json().get("data", None).get("children", None):
-            hot_list.append(i.get("data", None).get("title", None))
-        if after:
-            return (recurse(subreddit, hot_list, after))
-        return(hot_list)
-    else:
-        return (None)
+    if html.status_code != 200:
+        return None
+
+    data = html.json().get("data")
+    after = html.json().get("data").get("after")
+    children = data.get("children")
+    for i in children:
+        titles = i.get("data").get("title")
+        hot_list.append(titles)
+    if after:
+        return (recurse(subreddit, hot_list, after))
+    return(hot_list)
 
 
 if __name__ == "__main__":
