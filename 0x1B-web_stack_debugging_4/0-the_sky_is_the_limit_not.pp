1d23910080d152ea781c43ff99657956990d2019
bryanbuiles <1482@holbertonschool.com>
2020-10-06 14:52:03 -0500

    all tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# increases the limit for nginx
+exec { 'fix limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,5 @@
+# 0x1B. Web stack debugging #4
+
+## Learning objectives
+
+Try to improve the performance of the server
